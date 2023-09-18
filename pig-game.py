59ec65f04289fdcadf9b89718b50fe5564d5ee96
rafalos99 <rafsanzaniludhi@gmail.com>
2023-09-18 18:27:29 +0100

    written pseudo code for the rules of dice game

---
+++ pig-game.py
@@ -0,0 +1,33 @@
+# Initialize max_score to 50
+
+# Ask for the number of players
+# WHILE the number of players is not between 2 and 4
+#     Prompt the user to enter a correct number of players
+
+# Initialize an array player_scores with zeros based on the number of players
+
+# WHILE the max element in player_scores is less than max_score
+#     FOR each player in players
+#         Initialize current_score to 0
+#         Display the player's total score from player_scores
+        
+#         WHILE TRUE
+#             Ask the player if they want to roll
+#             IF player chooses not to roll
+#                 BREAK
+
+#             Roll a die and get the value
+#             IF the value is 1
+#                 Display "You rolled a 1! Turn done!"
+#                 Set current_score to 0
+#                 BREAK
+#             ELSE
+#                 Add value to current_score
+#                 Display the rolled value and current_score
+        
+#         Add current_score to the player's total score in player_scores
+#         Display the player's new total score
+
+# Find the max value in player_scores
+# Find the index of the max value in player_scores
+# Declare the player at that index as the winner
